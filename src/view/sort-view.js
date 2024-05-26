e7d660b1b0dc2ff04f95b6ca3ac2f9fa9055d376
Daria <dariykurochkina@mail.ru>
2024-05-26 22:11:42 +0500

    5.2

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -2,8 +2,23 @@ import AbstractView from '../framework/view/abstract-view.js';
 import {createSortTemplate} from '../template/sort-template.js';
 
 export default class SortView extends AbstractView{
+  #onSortTypeChange = null;
+
+  constructor({onSortTypeChange}) {
+    super();
+    this.#onSortTypeChange = onSortTypeChange;
+    this.element.addEventListener('click', this.#sortTypeChangeHandler);
+  }
+
   get template() {
     return createSortTemplate();
   }
 
+  #sortTypeChangeHandler = (evt) => {
+    if (evt.target.tagName !== 'INPUT') {
+      return;
+    }
+
+    this.#onSortTypeChange(evt.target.dataset.sortType);
+  };
 }
